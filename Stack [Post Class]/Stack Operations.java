631acad838dcec9a5dae2d92a0d4d0661c2c1f76
Javid Raina <rainajavid15@gmail.com>
2022-03-22 22:04:55 +0530

    fixed stack Operations

--- Stack [Post Class]/Stack Operations.java
+++ Stack [Post Class]/Stack Operations.java
@@ -1,3 +1,19 @@
-public class Stack Operations {
-     
-}
+public static void push(Stack<Integer> st, int x)
+    {
+        // your code here
+        st.push(x);
+    }
+
+    // Function to pop element from stack
+    public static void pop(Stack<Integer> st)
+    {
+        //your code here
+        if(!st.empty()) st.pop();
+    }
+
+    public static void top(Stack<Integer> st)
+    {
+        // your code here
+        if(st.empty()) System.out.println(0);
+        else System.out.println(st.peek());
+    }
